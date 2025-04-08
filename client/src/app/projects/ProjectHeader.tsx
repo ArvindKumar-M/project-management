741a421ae1931ceeadcaaab3c0b075e6c4e56f0c
AravindKumar-M <aravindkumarru@gmail.com>
2025-04-08 19:35:10 +0530

    UI: Updated button name

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -39,7 +39,7 @@ const ProjectHeader = ({
                 onClick={() => setIsModalNewProjectOpen(true)}
               >
                 <PlusSquare className="mr-2 h-5 w-5" />
-                New Boards
+                New Project
               </button>
               <button
                 className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
